2d6d9b12c1101d6ab4192768f19826d2e1fba390
EGregg <creosino@gmail.com>
2020-03-11 00:30:33 -0400

    Update SphereVolume.java

--- lab2/SphereVolume.java
+++ lab2/SphereVolume.java
@@ -1,21 +1,40 @@
 import java.util.Scanner;
 import java.lang.Math;
 
-public class SphereVolume 
-{ 
-public static void main(String[] args) 
-{ 
-Scanner stdin = new Scanner(System.in);
-double userDiameter,radius;
-double volumeSphere;
+/**
+ * Class: CMSC203 Spring 2020
+ * Program: Lab2 SphereVolume
+ * Instructor: Prof. Gregoriy Grinberg
+ * Description: a Java program to calculate volume of a sphere when a user inputs the diameter
+ * Due: 02/09/2020
+ * I pledge that I have completed the programming assignment independently.
+ * I have not copied the code from a student or any source.
+ * I have not given my code to any student.
+ * Print your Name here: Edward Gregg
+ */
 
-System.out.println("What is the diameter of a sphere");
-userDiameter = stdin.nextDouble();
-radius = (double)(userDiameter/2.0);
-//System.out.println(radius);
-volumeSphere = ((double)(4.0/3.0)*Math.PI*Math.pow(radius,3));
-//System.out.println((double)(4.0/3.0)*Math.PI);
+public class SphereVolume {
+	public static void main(String[] args) {
+		//creates new scanner object
+		Scanner stdin = new Scanner(System.in);
+		
+		//variable initialization
+		double userDiameter, radius;
+		double volumeSphere;
 
-System.out.println("Given a diameter of " + userDiameter + ", the volume of the sphere is " + volumeSphere);
-} 
-} 
+		//outputs to the terminal asking for the diameter and then accepts the next double entered from keyboard
+		System.out.println("What is the diameter of a sphere");
+		userDiameter = stdin.nextDouble();
+		
+		//the radius is 1/2 of the diameter of the circle
+		radius = (double) (userDiameter / 2.0);
+
+		//volume of the sphere is calculated using the formula 4/3 * PI * r ^ 2
+		volumeSphere = ((double) (4.0 / 3.0) * Math.PI * Math.pow(radius, 3));
+
+		//ouput displaying the input diameter and the calculated volume of sphere
+		System.out.println("Given a diameter of " + userDiameter + ", the volume of the sphere is " + volumeSphere);
+		
+		stdin.close();
+	}
+}
